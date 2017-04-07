252fa25ec131aefbf54852512434895a89bd4dd2
Andrew Nicholas Sipes <ansipes@dhcp-v511-203093.resnet-1x-wireless.unc.edu>
2017-04-07 11:51:27 -0400

    Created routes for Exercises, Foods, Hydrations, and HTML.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,46 @@
 |
 */
 
+// HTML Routes
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
+
+/*
+|--------------------------------------------------------------------------
+*/
+
+// Collection Routes
+
+//// Food
+Route::get('/foods', 'FoodsController@index');
+Route::post('/foods', 'FoodsController@create');
+
+//// Hydration
+Route::get('/hydrations', 'HydrationsController@index');
+Route::post('/hydrations', 'HydrationsController@create');
+
+//// Exercise
+Route::get('/exercises', 'ExercisesController@index');
+Route::post('/exercises', 'ExercisesController@create');
+
+/*
+|--------------------------------------------------------------------------
+*/
+
+// Singular Routes
+
+//// Food
+Route::get('/foods/{id}', 'FoodsController@show');
+Route::put('/foods/{id}', 'FoodsController@update');
+Route::delete('/foods/{id}', 'FoodsController@destroy');
+
+//// Hydration
+Route::get('/hydrations/{id}', 'HydrationsController@show');
+Route::put('/hydrations/{id}', 'HydrationsController@update');
+Route::delete('/hydrations/{id}', 'HydrationsController@destroy');
+
+//// Exercise
+Route::get('/exercises/{id}', 'ExercisesController@show');
+Route::put('/exercises/{id}', 'ExercisesController@update');
+Route::delete('/exercises/{id}', 'ExercisesController@destroy');
